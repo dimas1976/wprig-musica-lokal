765f5c2e4229fed74d5dd9522b5bbefd6b771c14
Dima <dm_kogan@yahoo.de>
2020-04-18 21:12:32 +0200

    neue forderer

--- foerderer.php
+++ foerderer.php
@@ -20,29 +20,48 @@ get_header();
             <div class="row">
                 <div class="col-md-4" data-aos="fade-right">
                     <picture>
-                        <source media="(min-width:1200px)" data-srcset="<?= bloginfo("template_url"); ?>/assets/img/foerderer/schaefer/schaefer_xl.jpg 3.5x">
-                        <source media="(min-width:992px)" data-srcset="<?= bloginfo("template_url"); ?>/assets/img/foerderer/schaefer/schaefer_lg.jpg 3x">
-                        <source media="(min-width:768px)" data-srcset="<?= bloginfo("template_url"); ?>/assets/img/foerderer/schaefer/schaefer_md.jpg 2x">
-                        <source media="(min-width:576px)" data-srcset="<?= bloginfo("template_url"); ?>/assets/img/foerderer/schaefer/schaefer_sm.jpg 1x">
-                        <source data-srcset="<?= bloginfo("template_url"); ?>/assets/img/foerderer/schaefer/schaefer_xs.jpg">
-                        <img class="img-fluid pt-md-3 lazyload" data-src="<?= bloginfo("template_url"); ?>/assets/img/foerderer/schaefer/schaefer_xl.jpg"  alt="Wir bedanken uns bei nachfolgenden Förderern-Birgit Schäfer">
+                        <source media="(min-width:1200px)" data-srcset="<?= bloginfo("template_url"); ?>/img/foerderer/entholt/entholt_xl.jpg 3.5x">
+                        <source media="(min-width:992px)" data-srcset="<?= bloginfo("template_url"); ?>/img/foerderer/entholt/entholt_lg.jpg 3x">
+                        <source media="(min-width:768px)" data-srcset="<?= bloginfo("template_url"); ?>/img/foerderer/entholt/entholt_md.jpg 2x">
+                        <source media="(min-width:576px)" data-srcset="<?= bloginfo("template_url"); ?>/img/foerderer/entholt/entholt_sm.jpg 1x">
+                        <source data-srcset="<?= bloginfo("template_url"); ?>/img/foerderer/entholt/entholt_xs.jpg">
+                        <img class="img-fluid pt-md-3 lazyload" data-src="<?= bloginfo("template_url"); ?>/img/foerderer/entholt/entholt_xl.jpg"  alt="Wir bedanken uns bei nachfolgenden Förderern-Birgit Schäfer">
                     </picture>
-                    <div class="credit">Foto: &copy; Kirsten Haarmann</div>
+                    <div class="credit">Foto: &copy; Rüdiger Ratsch-Heitmann</div>
                 </div>
                 <div class="col-md-8 pt-3 pb-3 pl-md-5" data-aos="fade-right">
                     <p>
-                        Der „Musica Altona e. V.“ wurde 2002 von der BürgerStiftung Hamburg (und einigen anderen Institutionen und Musikern) initiiert und wird seitdem von ihr maßgeblich unterstützt. Seit nunmehr fast 16 Jahren engagierter Arbeit der DozentInnen und vor allem auch des Vorstands gelingt es sehr erfolgreich, Kindern und Jugendlichen in Altona-Altstadt das gemeinsame Musizieren in einem interkulturellem Rahmen zu ermöglichen. Durch das positive Erlebnis beim gemeinschaftlichen Musikmachen fördert „Musica Altona“ soziale Kompetenzen, hilft Vorurteile abzubauen, trägt zum gegenseitigen Verständnis bei und fördert interkulturelle Freundschaften.
-                        „Menschen verbinden - Zukunft stiften“ so lautet das Motto der BürgerStiftung Hamburg seit der Gründung der Gemeinschaftsstiftung 1999 – in  diesem Projekt wird es auf besonders überzeugende Weise umgesetzt.<br/>
+                        Musica Altona e.V. wurde 2002 von mehreren Institutionen und Musikern*innen in Altona
+                        gemeinsam mit der BürgerStiftung Hamburg gegründet. Seitdem bieten die Dozentinnen und
+                        Dozenten, die sich mit dem Verein eng verbunden fühlen, Kindern und Jugendlichen
+                        Instrumentalunterricht hauptsächlich in Gruppen an, und das zu erschwinglichen Preisen. Förderer
+                        und eine Treuhandstiftung der BürgerStiftung Hamburg ermöglichen für einige auch die kostenlose
+                        Teilnahme.
+                         
+                        Die Geschichte, die man von Musica Altona erzählen könnte, handelt von der Liebe zur Musik und
+                        dem Vertrauen in die eigenen Fähigkeiten. Sie handelt von der Wertschätzung für die Kinder und
+                        Jugendlichen, die ein Instrument lernen wollen– Wertschätzung durch die Eltern und die Dozenten ist
+                        besonders wichtig. Die gemeinsamen Erlebnisse mit Freunden und verschiedenen Instrumenten, in
+                        Ensembles, Bands oder Gruppen zum Video-Dreh lassen einen besonderen Team-Geist spüren. Und
+                        die internationalen Namen der Protagonisten in den Geschichten von Musica Altona zeigen, wie bunt
+                        und vielfältig die Mischung der Kulturen in den musikalischen Gruppen ist.
+                         
+                        Keine Geschichte ohne Abenteuer! Für das Abenteuer des gemeinsamen Musizierens und die
+                        Umsetzung eigener Wünsche braucht man manchmal einfach Mut.
+                         
+                        Die BürgerStiftung Hamburg unterstützt Musica Altona e.V. seit fast 18 Jahren, weil wir immer
+                        wieder davon überzeugt werden, dass hier hochwertige musikalische und menschlich enorm
+                        fördernde Arbeit jedes Jahr aufs Neue geleistet wird.<br/>
                     </p>
                     <div class="container pl-0">
                         <div class="row">
                             <div class="col-md-6">
-                                Birgit Schäfer<br>
-                                Vorsitzende des Vorstands<br>
-                                BürgerStiftung Hamburg
+                                Dr. Dagmar Entholt-Laudien<br>
+                                Vorstandsvorsitzende BürgerStiftung Hamburg<br>
+                                Januar 2020
                             </div>
                             <div class="col-md-6 bsh-logo">
-                                <a href="http://www.buergerstiftung-hamburg.de/" title="zu Bürgerstiftung Hamburg" target="_blank"><img class="img-fluid lazyload" data-src="<?= bloginfo("template_url"); ?>/assets/img/logos/bsh.jpg" alt="Wir bedanken uns bei nachfolgenden Förderern-BürgerStiftung Hamburg"></a>
+                                <a href="http://www.buergerstiftung-hamburg.de/" title="zu Bürgerstiftung Hamburg" target="_blank"><img class="img-fluid lazyload" data-src="<?= bloginfo("template_url"); ?>/img/logos/bsh.jpg" alt="Wir bedanken uns bei nachfolgenden Förderern-BürgerStiftung Hamburg"></a>
                             </div>
                         </div>
                     </div>
@@ -54,27 +73,27 @@ get_header();
         <div class="container mb-5">
             <div class="row">
                 <div class="col-6 pb-4 col-md-3" data-aos="zoom-in">
-                    <a href="https://www.deutsches-stiftungszentrum.de/stiftungen/j%C3%BCrgen-und-monika-blankenburg-stiftung" target="_blank" title="Zu Jürgen und Monika Blankenburg Stiftung"><img class="img-fluid lazyload" data-src="<?= bloginfo("template_url"); ?>/assets/img/logos/b.jpg" alt="Wir bedanken uns bei nachfolgenden Förderern-Jürgen & Monika Blankenburg Stiftung"></a>
+                    <a href="https://www.deutsches-stiftungszentrum.de/stiftungen/j%C3%BCrgen-und-monika-blankenburg-stiftung" target="_blank" title="Zu Jürgen und Monika Blankenburg Stiftung"><img class="img-fluid lazyload" data-src="<?= bloginfo("template_url"); ?>/img/logos/b.jpg" alt="Wir bedanken uns bei nachfolgenden Förderern-Jürgen & Monika Blankenburg Stiftung"></a>
                 </div>
                 <div class="col-6 pb-4 col-md-3" data-aos="zoom-in">
-                    <a href="http://www.budnianer-hilfe.de/" title="zu Budnianer Hilfe e.V." target="_blank"><img class="img-fluid lazyload" data-src="<?= bloginfo("template_url"); ?>/assets/img/logos/buhi.jpg" alt="Wir bedanken uns bei nachfolgenden Förderern-Budnianer Hilfe e.V"></a>
+                    <a href="http://www.budnianer-hilfe.de/" title="zu Budnianer Hilfe e.V." target="_blank"><img class="img-fluid lazyload" data-src="<?= bloginfo("template_url"); ?>/img/logos/buhi.jpg" alt="Wir bedanken uns bei nachfolgenden Förderern-Budnianer Hilfe e.V"></a>
                 </div>
                 <div class="col-6 pb-4 col-md-3" data-aos="zoom-in">
-                    <a href="http://www.celloundco.de/" target="_blank" title="zu Cello & Co."><img class="img-fluid lazyload" data-src="<?= bloginfo("template_url"); ?>/assets/img/logos/cello.jpg" alt="Wir bedanken uns bei nachfolgenden Förderern-cello & co"></a>
+                    <a href="http://www.celloundco.de/" target="_blank" title="zu Cello & Co."><img class="img-fluid lazyload" data-src="<?= bloginfo("template_url"); ?>/img/logos/cello.jpg" alt="Wir bedanken uns bei nachfolgenden Förderern-cello & co"></a>
                 </div>
                 <div class="col-6 pb-4 col-md-3" data-aos="zoom-in">
-                    <a href="https://esche.eu/" title="zu die Esche in Hamburg Altona" target="_blank"><img class="img-fluid lazyload" data-src="<?= bloginfo("template_url"); ?>/assets/img/logos/esche.jpg" alt="Wir bedanken uns bei nachfolgenden Förderern-Die Esche in Hamburg Altona"></a>
+                    <a href="https://esche.eu/" title="zu die Esche in Hamburg Altona" target="_blank"><img class="img-fluid lazyload" data-src="<?= bloginfo("template_url"); ?>/img/logos/esche.jpg" alt="Wir bedanken uns bei nachfolgenden Förderern-Die Esche in Hamburg Altona"></a>
                 </div>
             </div>
             <div class="row">
                 <div class="col-6 pb-4 col-md-4" data-aos="zoom-in">
-                    <a href="http://www.ida-ehre-kulturverein.de/" target="_blank" title="zu Ida Ehre Kulturverein e.V."><img class="img-fluid lazyload" data-src="<?= bloginfo("template_url"); ?>/assets/img/logos/ida.jpg" alt="Wir bedanken uns bei nachfolgenden Förderern-Ida Ehre Kulturverein e.V."></a>
+                    <a href="http://www.ida-ehre-kulturverein.de/" target="_blank" title="zu Ida Ehre Kulturverein e.V."><img class="img-fluid lazyload" data-src="<?= bloginfo("template_url"); ?>/img/logos/ida.jpg" alt="Wir bedanken uns bei nachfolgenden Förderern-Ida Ehre Kulturverein e.V."></a>
                 </div>
                 <div class="col-6 pb-4 col-md-4" data-aos="zoom-in">
-                    <a href="https://www.telekom-stiftung.de/" title="zu Deutsche Telekom Stiftung"><img class="img-fluid lazyload" data-src="<?= bloginfo("template_url"); ?>/assets/img/logos/ikw.jpg" alt="Wir bedanken uns bei nachfolgenden Förderern-Deutsche Telekom Stiftung"></a>
+                    <a href="https://www.telekom-stiftung.de/" title="zu Deutsche Telekom Stiftung"><img class="img-fluid lazyload" data-src="<?= bloginfo("template_url"); ?>/img/logos/ikw.jpg" alt="Wir bedanken uns bei nachfolgenden Förderern-Deutsche Telekom Stiftung"></a>
                 </div>
                 <div class="col-6 pb-4 col-md-4" data-aos="zoom-in">
-                    <a href="http://stiftung-kinderjahre.de/" title="zu Stiftung Kinderjahre" target="_blank"><img class="img-fluid lazyload" data-src="<?= bloginfo("template_url"); ?>/assets/img/logos/kinderjahre.jpg" alt="Wir bedanken uns bei nachfolgenden Förderern-Stiftung Kinderjahre"></a>
+                    <a href="http://stiftung-kinderjahre.de/" title="zu Stiftung Kinderjahre" target="_blank"><img class="img-fluid lazyload" data-src="<?= bloginfo("template_url"); ?>/img/logos/kinderjahre.jpg" alt="Wir bedanken uns bei nachfolgenden Förderern-Stiftung Kinderjahre"></a>
                 </div>
             </div>
         </div>
