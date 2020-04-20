950a2299a973717fe2232250b3c23490dfaf1daa
Dima <dm_kogan@yahoo.de>
2020-04-20 21:11:11 +0200

    test new branch

--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -19,14 +19,6 @@
             <?php
             if ('post' === get_post_type()) {
                 echo '<div class="entry-meta">';
-/*                if (is_single()) {
-                    //twentyseventeen_posted_on();
-                } else {
-                    // echo twentyseventeen_time_link();
-                    //twentyseventeen_edit_link();
-
-
-                };*/
                 echo '</div><!-- .entry-meta -->';
             };
             ?>
