7f35ddc013ddb80b4d23f91b2743feae9e7ca5c8
Dima <dm_kogan@yahoo.de>
2021-12-19 19:29:53 +0100

    Solve caching problem

--- anniversary.php
+++ anniversary.php
@@ -4,52 +4,49 @@ Template Name: Anniversary
  */
 
 //get_header();
+
 ?>
 <!DOCTYPE html>
-<html>
-<head>
-	<title>Anniversary-Musica Altona</title>
-	<link rel="icon" type="image/png" href="<?php bloginfo('template_directory') ;?>/favicon/favicon-16x16.png"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-	<meta http-equiv="Pragma" content="no-cache" />
-	<meta http-equiv="Expires" content="0" />
-	<link rel="stylesheet" href="<?php bloginfo('template_directory') ;?>/css/styles.min.css">
-	<script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.4.0/dist/confetti.browser.min.js"></script>
-</head>
+	<html>
+		<head>
+			<title>Anniversary-Musica Altona</title>
+			<link rel="icon" type="image/png" href="<?php bloginfo('template_directory') ;?>/favicon/favicon-16x16.png"/>
+			<link rel="stylesheet" href="<?php bloginfo('template_directory') ;?>/css/styles.min.css">
+			<script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.4.0/dist/confetti.browser.min.js"></script>
+		</head>
 
-<body>
-<div class="container anniversary-container">
-	<img class="anniversary-container__image" src="<?php bloginfo('template_directory');?>/img/anniversary/teaser.png" alt="teaser">
-	<a class="anniversary-container__link" href="https://dev.musica-altona.de/kinder-foerdern-durch-musik">ZUR STARTSEITE</a>
-</div>
-<script>
-	// do this for 30 seconds
-	var duration = 0.3 * 1000;
-	var end = Date.now() + duration;
+		<body>
+			<div class="container anniversary-container">
+				<img class="anniversary-container__image" src="<?php bloginfo('template_directory');?>/img/anniversary/teaser.png?123" alt="teaser">
+				<a class="anniversary-container__link" href="https://dev.musica-altona.de/kinder-foerdern-durch-musik">ZUR STARTSEITE</a>
+			</div>
+			<script>
+				// do this for 30 seconds
+				var duration = 0.3 * 1000;
+				var end = Date.now() + duration;
 
-	(function frame() {
-		// launch a few confetti from the left edge
-		confetti({
-			particleCount: 60,
-			spread: 300,  origin: {
-				x: Math.random(),
-				// since they fall down, start a bit higher than random
-				y: Math.random() - 0.2
-			}
-		});
-		// and launch a few from the right edge
+				(function frame() {
+					// launch a few confetti from the left edge
+					confetti({
+						particleCount: 60,
+						spread: 300,  origin: {
+							x: Math.random(),
+							// since they fall down, start a bit higher than random
+							y: Math.random() - 0.2
+						}
+					});
+					// and launch a few from the right edge
 
-		// keep going until we are out of time
-		if (Date.now() < end) {
-			requestAnimationFrame(frame);
-		}
-	}());
+					// keep going until we are out of time
+					if (Date.now() < end) {
+						requestAnimationFrame(frame);
+					}
+				}());
 
-</script>
-</body>
+			</script>
+		</body>
 
-</html>
+	</html>
 
 
 
