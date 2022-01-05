e8563a40aa2906557de86e9c7b34915e1e86bca2
Dima <dm_kogan@yahoo.de>
2022-01-05 20:03:49 +0100

    Add videos to 20 years site

--- 20_years.php
+++ 20_years.php
@@ -5,5 +5,10 @@ Template Name: 20Years
 
 get_header();
 ?>
-
+<section class="years">
+	<div class="years__wrapper container">
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/n_g2E_aTm2g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/7fTLla8w3Kk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></iframe>
+	</div>
+</section>
 <?php get_footer();?>
