ddcd021c646193be9e38d3434ce1ace014fc74f6
Dima <dm_kogan@yahoo.de>
2022-05-05 13:36:27 +0200

    Change order of articles

--- 20_years.php
+++ 20_years.php
@@ -8,12 +8,19 @@ get_header();
 <section class="years">
 	<div class="years__wrapper container">
 		<article class="video" data-aos="fade-left">
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/n_g2E_aTm2g"
+			<iframe width="560" height="315" src="https://youtube.com/embed/UqiRtTTh_pM"
 					title="YouTube video player" frameborder="0"
 					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 					allowfullscreen></iframe>
 		</article>
-		<article class="video" data-aos="fade-left">
+		<article class="no-video" data-aos="fade-right">
+			<h2>Gruß von der ESCHE zum Jubiläum</h2>
+			<p>
+				"Wir wünschen Euch von Herzen alles Gute zum Geburtstag! 20 Jahre – wow! Bewundernswert, was Ihr in dieser Zeit auf die Beine gestellt habt."<br>
+				<a href="https://esche.eu/hoch-die-tassen/">Hier geht es zu dem ganzen Artikel</a>
+			</p>
+		</article>
+		<article class="video mb-3" data-aos="fade-left">
 			<iframe width="560" height="315" src="https://www.youtube.com/embed/7fTLla8w3Kk"
 					title="YouTube video player" frameborder="0"
 					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -21,17 +28,10 @@ get_header();
 			</iframe>
 		</article >
 		<article class="video" data-aos="fade-left">
-			<iframe width="560" height="315" src="https://youtube.com/embed/UqiRtTTh_pM"
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/n_g2E_aTm2g"
 					title="YouTube video player" frameborder="0"
 					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 					allowfullscreen></iframe>
-		</article>
-		<article data-aos="fade-right">
-			<h2>Gruß von der ESCHE zum Jubiläum</h2>
-			<p>
-				"Wir wünschen Euch von Herzen alles Gute zum Geburtstag! 20 Jahre – wow! Bewundernswert, was Ihr in dieser Zeit auf die Beine gestellt habt."<br>
-				<a href="https://esche.eu/hoch-die-tassen/">Hier geht es zu dem ganzen Artikel</a>
-			</p>
 		</article>
 	</div>
 </section>
