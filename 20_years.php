a879c3b9a1eb9c1a5df062b3022eb6f64f2ce73b
Dima <dm_kogan@yahoo.de>
2022-06-22 13:41:59 +0200

    Add ndr report

--- 20_years.php
+++ 20_years.php
@@ -6,7 +6,18 @@
 get_header();
 ?>
 <section class="years">
+
 	<div class="years__wrapper container">
+		<article class="image mb-3" data-aos="zoom-in">
+			<figure class="ndr-report">
+				<img src="<?=bloginfo("template_url"); ?>/img/anniversary/ndr_logo.webp" alt="NDR-Logo">
+				<figcaption>NDR 90,3 Kulturjournal (zu hören immer wochentags zwischen 19 und 20 Uhr) - eine tolle Reportage von Petra Volquardsen zu unserem Jubiläum. Viel Spaß beim Hören...</figcaption>
+				<audio controls src="<?=bloginfo("template_url"); ?>/audio/mitschnitt.mp3"></audio>
+			</figure>
+		</article>
+		<div class="divider mt-4 mb-4" data-aos="zoom-in">
+			<img src="<?=bloginfo("template_url"); ?>/img/anniversary/luftballons.jpg" alt="">
+		</div>
 		<article class="image mb-3" data-aos="zoom-in">
 			<a href="https://musica-altona.de/wp-content/uploads/2022/06/klaenge.pdf" target="_blank"><img src="<?=bloginfo("template_url"); ?>/img/anniversary/klaenge_n.jpg" alt="Anzeige zur Veranstaltung"></a>
 		</article>
