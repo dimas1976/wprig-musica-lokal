8171826a077f1144ae5c5ea80dbf5281a6d0d852
Dima <dm_kogan@yahoo.de>
2022-07-29 14:41:06 +0200

    Set names after qoutes

--- about.php
+++ about.php
@@ -5,15 +5,15 @@ Template Name: Ueber Uns - statische Seite
 get_header();
 ?>
 <div id="motiv-about" class="mb-3" data-aos="fade-down">
-    <div class="credit">Foto: &copy; Fayssal Boukari</div>
+	<div class="credit">Foto: &copy; Fayssal Boukari</div>
 </div>
 
 <section id="about-section">
-    <div class="container pt-5  pl-4 pr-4 pl-md-3 pr-md-3 pl-lg-0 pr-lg-0 ">
-        <div class="row">
-            <div class="col-sm-12">
-                <h2 class="pb-3" data-aos="zoom-in">Ein vielseitiges Musikprojekt in der Nachbarschaft</h2>
-                <p class="pb-3" data-aos="zoom-in-up">
+	<div class="container pt-5  pl-4 pr-4 pl-md-3 pr-md-3 pl-lg-0 pr-lg-0 ">
+		<div class="row">
+			<div class="col-sm-12">
+				<h2 class="pb-3" data-aos="zoom-in">Ein vielseitiges Musikprojekt in der Nachbarschaft</h2>
+				<p class="pb-3" data-aos="zoom-in-up">
 					Der Musica Altona e. V. wurde 2002 von der BürgerStiftung Hamburg zusammen mit einigen
 					Institutionen und engagierten Musiker:innen in Altona-Altstadt initiiert, um Kindern und
 					Jugendlichen in einem interkulturellen Rahmen das Musizieren zu ermöglichen. Ohne die
@@ -29,74 +29,88 @@ get_header();
 					Musica Altona versteht sich zudem als Plattform des sozialen und künstlerischen Austausches
 					in einer multikulturellen Stadt, die eine weltoffene Persönlichkeitsbildung der Einzelnen
 					unterstützt.<br>
-					<span class="about-section__quote">Marion Wolf-Dietrich (Vorstand) </span>:<br>
 					„Vor 20 Jahren traf sich eine Gruppe unterschiedlicher Menschen drüben in der KITA
 					Hospitalstraße im August-Lüttgens Park und gründete Musica Altona.
 					Die Idee dabei war, Kindern eine Freizeitalternative zu bieten. Damals sah es nämlich
 					in der Nachmittags-und Freizeitbetreuung noch sehr mager aus. Viele Kinder und Jugendliche
 					„daddelten“ ziellos durch den Stadtteil oder versumpften zu Hause alleine vor den
-					Fernsehern, guckten Videos...“
-                </p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-12">
-                <h2 class="pb-3" data-aos="zoom-in">Die Kraft der Musik</h2>
-                <p data-aos="zoom-in-up">
-					<span class="about-section__quote">Patricia Renz (Vorstandsvorsitz)</span>:<br>
+					Fernsehern, guckten Videos...“<br>
+					<span class="about-section__quote">Marion Wolf-Dietrich (Vorstand) </span>
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12">
+				<h2 class="pb-3" data-aos="zoom-in">Die Kraft der Musik</h2>
+				<p data-aos="zoom-in-up">
 					„Wir von Musica Altona möchten Kindern ein Fenster zur Musik öffnen, die sonst keinen
 					Zugang dazu hätten. Dabei ist es uns wichtig, mit Spaß und Freude am Musizieren
 					Kinder und Jugendliche in ihrem Selbstvertrauen zu stärken. Jeder Auftritt und der Applaus,
 					aber auch schon die kleinen Erfolge im Unterricht, bringen die Kinder voran – z.B. wenn es
 					plötzlich nach einigem Üben 'klingt' und manchmal sogar richtig einen 'Flow' schafft. Wir sind
 					überzeugt, dass die Kraft der Musik und des gemeinsamen Musizierens etwas Besonderes ist.“<br>
+					<span class="about-section__quote">Patricia Renz (Vorstandsvorsitz)</span>
+				</p>
+				<p>
 					Unter der Leitung von erfahrenen Dozenten:innen und Musikpädagogen:innen treffen sich die
 					Schüler:innen in Altonaer Schulen und anderen Einrichtungen. Auf einen einzigen Musikstil
 					müssen sich die inzwischen über 300 Nachwuchsmusiker:innen nicht festlegen, denn bei
 					Musica Altona wird in verschiedenen Gruppen »Crossover« musiziert:
 					von Schlagzeug, Blasinstrumenten, Klavier, Keyboard, afrikanischen Trommeln über Saz,
 					Ukulele und Gitarre bis zur klassischen Geige, Cello sowie einem Shanty Chor und
-					Band Projekten.<br>
-					<span class="about-section__quote">Bethina Walbaum (Vorstand)</span>:<br>
+					Band Projekten.
+				</p>
+				<p>
 					„Ob Elternstunde oder unser KinderMusikfest - für manche Kinder ist es der erste Auftritt,
 					andere sind schon länger dabei. Bei allem steht aber immer der Spaß an erster Stelle. Es geht
 					nicht um eine perfekte Darbietung, sondern um den Mut aufzutreten und etwas von dem
 					Erlernten zeigen zu wollen.“<br>
+					<span class="about-section__quote">Bethina Walbaum (Vorstand)</span>
+				</p>
+				<p>
 					Mit dem 'Rollenden Musikkoffer', der regelmäßig durch die Kitas des Stadtteils zieht, erleben
 					schon die ganz Kleinen seit vielen Jahren ein lebendiges Programm der musikalischen
 					Früherziehung. Anfang 2019 kam 'Bellas Klangtonne' dazu, ein Projekt für die Elementar
 					Kinder in Kitas, im März 2022 folgte 'Bella spielt Ukulele'.
 					Bereits hier wird das Prinzip des Gruppenunterrichts gepflegt, der sich dann für Kinder ab dem
-						ersten Schuljahr in den Instrumental- und natürlich auch Orchestergruppen fortsetzt.
-
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-12">
-                <h2 class="pb-3" data-aos="zoom-in">zu guter Letzt</h2>
-                <p class="pb-3" data-aos="zoom-in-up">
+					ersten Schuljahr in den Instrumental- und natürlich auch Orchestergruppen fortsetzt.
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12">
+				<h2 class="pb-3" data-aos="zoom-in">zu guter Letzt</h2>
+				<p class="pb-3" data-aos="zoom-in-up">
 					Seit 2002 ist der große und von Herzen kommende Einsatz aller Beteiligten bezeichnend für
 					Musica Altona. Die Kursgebühren sind moderat und die Organisation schlank.
 					So wird es jungen Menschen ermöglicht - auch denjenigen, die neu in der Stadt sind - einen
 					leichteren Zugang zu ihrem Stadtteil und anderen Jugendlichen zu finden.
-                </p>
-            </div>
-        </div>
-        <div class="row pb-5">
-            <div class="col-sm col-md-6 mt-4 mb-4 about-links-container">
-                    <a class="mb-2" href="./vorstand" title="Zur Vorstand-Seite">der Vorstand von Musica Altona <i class="fas fa-chevron-circle-right ml-2"></i></a>
-                    <a href="./geschichte" title="Zur Geschichte-Seite">die Gründungsgeschichte von Musica Altona <i class="fas fa-chevron-circle-right ml-2"></i></a>
-            </div>
-            <div class="col-sm-4 offset-sm-2 col-md-3 offset-md-3 col-lg-2 offset-lg-4">
-                <picture>
-                    <source media="(min-width:1200px)" data-srcset="<?= bloginfo("template_url"); ?>/img/refugess.gif 3.5x">
-                    <source media="(min-width:992px)" data-srcset="<?= bloginfo("template_url"); ?>/img/refugess.gif 3x">
-                    <source media="(min-width:768px)" data-srcset="<?= bloginfo("template_url"); ?>/img/refugess.gif 2x">
-                    <source media="(min-width:576px)" data-srcset="<?= bloginfo("template_url"); ?>/img/refugess.gif 1x">
-                    <source data-srcset="<?= bloginfo("template_url"); ?>/img/refugess.gif">
-                    <img data-src="<?= bloginfo("template_url"); ?>/img/refugess.gif" class="img-fluid lazyload refugees-image" alt="Bewährtes Konzept: Musizieren in der Gruppe macht stark" data-aos="fade-right">
-                </picture>
-            </div>
+				</p>
+			</div>
+		</div>
+		<div class="row pb-5">
+			<div class="col-sm col-md-6 mt-4 mb-4 about-links-container">
+				<a class="mb-2" href="./vorstand" title="Zur Vorstand-Seite">der Vorstand von Musica Altona <i
+						class="fas fa-chevron-circle-right ml-2"></i></a>
+				<a href="./geschichte" title="Zur Geschichte-Seite">die Gründungsgeschichte von Musica Altona <i
+						class="fas fa-chevron-circle-right ml-2"></i></a>
+			</div>
+			<div class="col-sm-4 offset-sm-2 col-md-3 offset-md-3 col-lg-2 offset-lg-4">
+				<picture>
+					<source media="(min-width:1200px)"
+							data-srcset="<?= bloginfo("template_url"); ?>/img/refugess.gif 3.5x">
+					<source media="(min-width:992px)"
+							data-srcset="<?= bloginfo("template_url"); ?>/img/refugess.gif 3x">
+					<source media="(min-width:768px)"
+							data-srcset="<?= bloginfo("template_url"); ?>/img/refugess.gif 2x">
+					<source media="(min-width:576px)"
+							data-srcset="<?= bloginfo("template_url"); ?>/img/refugess.gif 1x">
+					<source data-srcset="<?= bloginfo("template_url"); ?>/img/refugess.gif">
+					<img data-src="<?= bloginfo("template_url"); ?>/img/refugess.gif"
+						 class="img-fluid lazyload refugees-image"
+						 alt="Bewährtes Konzept: Musizieren in der Gruppe macht stark" data-aos="fade-right">
+				</picture>
+			</div>
 </section>
 <?php
 get_footer();
