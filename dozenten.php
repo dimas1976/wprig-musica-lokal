a230c39006b03e093ea7ee22bdc94b6dd32ff4ab
Dima <dm_kogan@yahoo.de>
2022-10-04 21:55:02 +0200

    Change order of articles

--- dozenten.php
+++ dozenten.php
@@ -1,6 +1,6 @@
 <?php
 /**
-Template Name: Dozenten
+ * Template Name: Dozenten
  */
 
 get_header(); ?>
@@ -9,12 +9,15 @@ get_header(); ?>
 		<div class="lecturer-container">
 			<div class="lecturer-wrapper lecturer-wrapper--dark">
 				<div class="lecturer-wrapper__image">
-					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/serena.jpg" class="img-fluid lazyload" alt="Serena Kahnert">
+					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/serena.jpg" class="img-fluid lazyload"
+						 alt="Serena Kahnert">
 				</div>
 				<div class="lecturer-wrapper__quote pt-2 pt-md-5 pb-5 pb-md-3">
 					<blockquote>
 						<sup class="pr-2"><i class="fas fa-quote-left"></i></sup>
-						Musica Altona bietet Kindern und Jugendlichen eine tolle Chance, ihre Lebensqualität durch das Instrumentalspiel deutlich zu verbessern. Großes Engagement auf allen Seiten.<sub class="pl-2"><i class="fas fa-quote-right"></i></sub>
+						Musica Altona bietet Kindern und Jugendlichen eine tolle Chance, ihre Lebensqualität durch das
+						Instrumentalspiel deutlich zu verbessern. Großes Engagement auf allen Seiten.<sub
+							class="pl-2"><i class="fas fa-quote-right"></i></sub>
 					</blockquote>
 					<p class="lecturer--light">Serena Kahnert</p>
 				</div>
@@ -23,46 +26,95 @@ get_header(); ?>
 				<div class="lecturer-wrapper__quote order-last order-md-first pt-2 pt-md-5 pb-5 pb-md-3 text-left">
 					<blockquote>
 						<sup class="pr-2"><i class="fas fa-quote-left"></i></sup>
-						Musica Altona bietet musikalische aber auch soziale Möglichkeiten, mit denen Kinder aufwachsen sollten.
+						Musica Altona bietet musikalische aber auch soziale Möglichkeiten, mit denen Kinder aufwachsen
+						sollten.
 						<sub class="pl-2"><i class="fas fa-quote-right"></i></sub>
 					</blockquote>
 					<p class="lecturer--dark">Thando Walbaum</p>
 				</div>
 				<div class="lecturer-wrapper__image order-first order-md-last">
-					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/thando.jpg" class="img-fluid lazyload" alt="Thando Walbaum">
+					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/thando.jpg" class="img-fluid lazyload"
+						 alt="Thando Walbaum">
 					<div class="credit">Foto: &copy; Thomas Leidig</div>
 				</div>
 			</div>
 			<div class="lecturer-wrapper lecturer-wrapper--dark">
 				<div class="lecturer-wrapper__image">
-					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/heidi.jpg" class="img-fluid heidi lazyload" alt="Heidi Martens">
+					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/gino.jpg"
+						 class="img-fluid lazyload" alt="Gino Ramirez">
 				</div>
 				<div class="lecturer-wrapper__quote pt-2 pt-md-5 pb-5 pb-md-3">
 					<blockquote>
 						<sup class="pr-2"><i class="fas fa-quote-left"></i></sup>
-						Musica Altona als Brücke, eine besondere Form der Kommunikation – unabhängig vom Alter oder kulturellem Hintergrund, Musica Altona verbindet <sub class="pl-2"><i class="fas fa-quote-right"></i></sub>
+						Wir bleiben dran! Es lohnt sich! Wir sind schon lange zusammen als 'Musikfreunde' <sub
+							class="pl-2"><i class="fas fa-quote-right"></i></sub>
 					</blockquote>
-					<p class="lecturer--light">Heidi Martens und Birgit Lünsmann </p>
+					<p class="lecturer--light">Gino Ramirez</p>
 				</div>
 			</div>
 			<div class="lecturer-wrapper lecturer-wrapper--light">
 				<div class="lecturer-wrapper__quote order-last order-md-first pt-2 pt-md-5 pb-5 pb-md-3">
 					<blockquote>
-						<sup class="pr-2"><i class="fas fa-quote-left"></i></sup>Musica Altona fühlt sich an wie
-						Elektrizität <sub class="pl-2"><i
+						<sup class="pr-2"><i class="fas fa-quote-left"></i></sup>
+						die Kinder kommen aus vielen Richtungen und Schulen. Beim Unterricht brauchen sie kein Handy.
+						Die Zeit vergeht so schnell und am Ende der Stunde gibt es einen guten, gemeinsamen Sound. <sub
+							class="pl-2"><i
 								class="fas fa-quote-right"></i></sub>
 					</blockquote>
-					<p class="lecturer--dark">Pablo Ruiz</p>
+					<p class="lecturer--dark">Krischa Weber</p>
 				</div>
 				<div class="lecturer-wrapper__image order-first order-md-last">
-					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/pablo.jpg" class="img-fluid pablo lazyload" alt="Pablo Ruiz">
+					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/krischa.jpg" class="img-fluid lazyload"
+						 alt="Krischa Weber">
+					<div class="credit">Foto: &copy; Eibe Krebs</div>
+				</div>
+			</div>
+			<div class="lecturer-wrapper lecturer-wrapper--dark">
+					<div class="lecturer-wrapper__image">
+						<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/pablo.jpg" class="img-fluid lazyload" alt="Pablo Ruiz">
+					</div>
+					<div class="lecturer-wrapper__quote pt-2 pt-md-5 pb-5 pb-md-3">
+						<blockquote>
+							<sup class="pr-2"><i class="fas fa-quote-left"></i></sup>Musica Altona fühlt sich an wie
+							Elektrizität <sub class="pl-2"><i class="fas fa-quote-right"></i></sub>
+						</blockquote>
+						<p class="lecturer--dark">Pablo Ruiz</p>
+					</div>
+
+			</div>
+			<div class="lecturer-wrapper lecturer-wrapper--light">
+				<div class="lecturer-wrapper__quote order-last order-md-first pt-2 pt-md-5 pb-5 pb-md-3">
+					<blockquote>
+						<sup class="pr-2"><i class="fas fa-quote-left"></i></sup>
+						Ich konnte gar nicht anders, als das Angebot anzunehmen, Teil dieser schönen, respektvollen
+						Initiative zu sein; bei der auch viele Talente gefördert werden, von denen wir vielleicht nie
+						etwas gehört hätten, wenn Musica Altona das nicht möglich gemacht hätte. <sub class="pl-2"><i
+								class="fas fa-quote-right"></i></sub>
+					</blockquote>
+					<p class="lecturer--dark">Nima Asadollahi Ali</p>
+				</div>
+				<div class="lecturer-wrapper__image order-first order-md-last">
+					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/nima.jpg" class="img-fluid lazyload"
+						 alt="Jan Feddersen">
 				</div>
 			</div>
 			<div class="lecturer-wrapper lecturer-wrapper--dark">
 				<div class="lecturer-wrapper__image">
-					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/olle.jpg" class="img-fluid lazyload" alt="Olle Wolski">
+					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/heidi.jpg"
+						 class="img-fluid heidi lazyload" alt="Heidi Martens">
 				</div>
 				<div class="lecturer-wrapper__quote pt-2 pt-md-5 pb-5 pb-md-3">
+					<blockquote>
+						<sup class="pr-2"><i class="fas fa-quote-left"></i></sup>
+						Musica Altona als Brücke, eine besondere Form der Kommunikation – unabhängig vom Alter oder
+						kulturellem Hintergrund, Musica Altona verbindet <sub class="pl-2"><i
+								class="fas fa-quote-right"></i></sub>
+					</blockquote>
+					<p class="lecturer--light">Heidi Martens und Birgit Lünsmann </p>
+				</div>
+			</div>
+			<div class="lecturer-wrapper lecturer-wrapper--light">
+				<div class="lecturer-wrapper__quote pt-2 pt-md-5 pb-5 pb-md-3 order-last order-md-first">
 					<blockquote>
 						<sup class="pr-2"><i class="fas fa-quote-left"></i></sup> Musica Altona ist sozial, gutes
 						Team, in meinem Stadtteil Altona,
@@ -73,97 +125,72 @@ get_header(); ?>
 					</blockquote>
 					<p class="lecturer--light">Olle Wolski</p>
 				</div>
-			</div>
-			<div class="lecturer-wrapper lecturer-wrapper--light">
-				<div class="lecturer-wrapper__quote pt-2 pt-md-5 pb-5 pb-md-3 order-last order-md-first">
-					<blockquote>
-						<sup class="pr-2"><i class="fas fa-quote-left"></i></sup>Musica Altona: in einem tollen Team bei Kindern die Freude an Musik fördern!<sub class="pl-2"><i
-								class="fas fa-quote-right"></i></sub>
-					</blockquote>
-					<p class="lecturer--dark">Willie Jakob</p>
-				</div>
 				<div class="lecturer-wrapper__image order-first order-md-last">
-					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/willi.jpg" class="img-fluid willi lazyload" alt="Willie Jakob">
+					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/olle.jpg" class="img-fluid lazyload"
+						 alt="Olle Wolski">
 				</div>
 			</div>
 			<div class="lecturer-wrapper lecturer-wrapper--dark">
-				<div class="lecturer-wrapper__image">
-					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/oliver.jpg" class="img-fluid lazyload" alt="Oli Höppner">
-					<div class="credit">Foto: &copy; EleonoraCucina</div>
-				</div>
+				<div class="lecturer-wrapper__image ">
+						<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/willi.jpg"
+							 class="img-fluid willi lazyload" alt="Willie Jakob">
+					</div>
 				<div class="lecturer-wrapper__quote pt-2 pt-md-5 pb-5 pb-md-3">
+
 					<blockquote>
-						<sup class="pr-2"><i class="fas fa-quote-left"></i></sup> die Kinder kommen freiwillig zum Unterricht.
+						<sup class="pr-2"><i class="fas fa-quote-left"></i></sup>Musica Altona: in einem tollen Team bei
+						Kindern die Freude an Musik fördern!<sub class="pl-2"><i
+								class="fas fa-quote-right"></i></sub>
+					</blockquote>
+					<p class="lecturer--dark">Willie Jakob</p>
+					</div>
+			</div>
+			<div class="lecturer-wrapper lecturer-wrapper--light lecturer-wrapper--space-between">
+				<div class="lecturer-wrapper__quote pt-2 pt-md-5 pb-5 pb-md-3 order-last order-md-first">
+					<blockquote>
+						<sup class="pr-2"><i class="fas fa-quote-left"></i></sup> die Kinder kommen freiwillig zum
+						Unterricht.
 						Das spricht für sich. <sub class="pl-2"><i class="fas fa-quote-right"></i></sub>
 					</blockquote>
 					<p class="lecturer--light">Oli Höppner</p>
 				</div>
-			</div>
-			<div class="lecturer-wrapper lecturer-wrapper--light">
-				<div class="lecturer-wrapper__quote order-last order-md-first pt-2 pt-md-5 pb-5 pb-md-3">
-					<blockquote>
-						<sup class="pr-2"><i class="fas fa-quote-left"></i></sup>
-						die Kinder kommen aus vielen Richtungen und Schulen. Beim Unterricht brauchen sie kein Handy. Die Zeit vergeht so schnell und am Ende der Stunde gibt es einen guten, gemeinsamen Sound. <sub class="pl-2"><i
-								class="fas fa-quote-right"></i></sub>
-					</blockquote>
-					<p class="lecturer--dark">Krischa Weber</p>
-				</div>
 				<div class="lecturer-wrapper__image order-first order-md-last">
-					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/krischa.jpg" class="img-fluid lazyload" alt="Krischa Weber">
-					<div class="credit">Foto: &copy; Eibe Krebs</div>
+					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/oliver.jpg" class="img-fluid lazyload"
+						 alt="Oli Höppner">
+					<div class="credit">Foto: &copy; EleonoraCucina</div>
 				</div>
 			</div>
 			<div class="lecturer-wrapper lecturer-wrapper--dark">
 				<div class="lecturer-wrapper__image">
-					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/gino.jpg" class="img-fluid gino lazyload" alt="Gino Ramirez">
+					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/jan.jpg" class="img-fluid lazyload"
+						 alt="Jan Feddersen">
 				</div>
 				<div class="lecturer-wrapper__quote pt-2 pt-md-5 pb-5 pb-md-3">
 					<blockquote>
 						<sup class="pr-2"><i class="fas fa-quote-left"></i></sup>
-						Wir bleiben dran! Es lohnt sich! Wir sind schon lange zusammen als 'Musikfreunde' <sub class="pl-2"><i class="fas fa-quote-right"></i></sub>
-					</blockquote>
-					<p class="lecturer--light">Gino Ramirez</p>
-				</div>
-			</div>
-			<div class="lecturer-wrapper lecturer-wrapper--light">
-				<div class="lecturer-wrapper__quote order-last order-md-first pt-2 pt-md-5 pb-5 pb-md-3">
-					<blockquote>
-						<sup class="pr-2"><i class="fas fa-quote-left"></i></sup>
-						In Musica Altona vereinen sich ein sozialer und ein künstlerischer Anspruch. Im Zentrum steht die Freude am gemeinsamen Musizieren. Das finde ich wunderbar und bin sehr froh, dass ich dabei bin. <sub class="pl-2"><i
+						In Musica Altona vereinen sich ein sozialer und ein künstlerischer Anspruch. Im Zentrum steht
+						die Freude am gemeinsamen Musizieren. Das finde ich wunderbar und bin sehr froh, dass ich dabei
+						bin. <sub class="pl-2"><i
 								class="fas fa-quote-right"></i></sub>
 					</blockquote>
 					<p class="lecturer--dark">Jan Feddersen</p>
 				</div>
-				<div class="lecturer-wrapper__image order-first order-md-last">
-					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/jan.jpg" class="img-fluid lazyload" alt="Jan Feddersen">
-				</div>
 			</div>
-			<div class="lecturer-wrapper lecturer-wrapper--dark">
-				<div class="lecturer-wrapper__image">
-					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/huseyin.jpg" class="img-fluid lazyload" alt="Hueseyin Duman">
-				</div>
-				<div class="lecturer-wrapper__quote pt-2 pt-md-5 pb-5 pb-md-3">
+			<div class="lecturer-wrapper lecturer-wrapper--light">
+				<div class="lecturer-wrapper__quote pt-2 pt-md-5 pb-5 pb-md-3 order-last order-md-first">
 					<blockquote>
 						<sup class="pr-2"><i class="fas fa-quote-left"></i></sup>
 
-						Musica Altona ist wie eine Musikalische Reise um die Welt. Wie ein Regenbogen, der uns immer wieder verzaubert!
+						Musica Altona ist wie eine Musikalische Reise um die Welt. Wie ein Regenbogen, der uns immer
+						wieder verzaubert!
 
 						<sub class="pl-2"><i class="fas fa-quote-right"></i></sub>
 					</blockquote>
 					<p class="lecturer--light">Hüseyin Duman</p>
 				</div>
-			</div>
-			<div class="lecturer-wrapper lecturer-wrapper--light">
-				<div class="lecturer-wrapper__quote order-last order-md-first pt-2 pt-md-5 pb-5 pb-md-3">
-					<blockquote>
-						<sup class="pr-2"><i class="fas fa-quote-left"></i></sup>
-						Ich konnte gar nicht anders, als das Angebot anzunehmen, Teil dieser schönen, respektvollen Initiative zu sein; bei der auch viele Talente gefördert werden, von denen wir vielleicht nie etwas gehört hätten, wenn Musica Altona das nicht möglich gemacht hätte. <sub class="pl-2"><i
-								class="fas fa-quote-right"></i></sub>
-					</blockquote>
-					<p class="lecturer--dark">Nima Asadollahi Ali</p>
-				</div>
 				<div class="lecturer-wrapper__image order-first order-md-last">
-					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/nima.jpg" class="img-fluid lazyload" alt="Jan Feddersen">
+					<img data-src="<?= bloginfo("template_url"); ?>/img/dozenten/huseyin.jpg" class="img-fluid lazyload"
+						 alt="Hueseyin Duman">
 				</div>
 			</div>
 		</div>
@@ -171,4 +198,4 @@ get_header(); ?>
 </div>
 
 
-<?php get_footer();?>
+<?php get_footer(); ?>
