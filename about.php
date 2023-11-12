8402e35746a9c72a33b3675246ff9fe77112a494
Dima <dm_kogan@yahoo.de>
2023-11-12 12:43:36 +0100

    Update text

--- about.php
+++ about.php
@@ -14,28 +14,12 @@ get_header();
 			<div class="col-sm-12">
 				<h2 class="pb-3" data-aos="zoom-in">Ein vielseitiges Musikprojekt in der Nachbarschaft</h2>
 				<p class="pb-3" data-aos="zoom-in-up">
-					Der Musica Altona e. V. wurde 2002 von der BürgerStiftung Hamburg zusammen mit einigen
-					Institutionen und engagierten Musiker:innen in Altona-Altstadt initiiert, um Kindern und
-					Jugendlichen in einem interkulturellen Rahmen das Musizieren zu ermöglichen. Ohne die
-					kontinuierliche Unterstützung von der Bürgerstiftung Hamburg hätte es dieses Projekt nicht
-					geben können. Ein besonderer Dank an Hugbert Flitner, der sich damals unermüdlich auf allen
-					Ebenen dafür einsetzte - bei der Stiftung, aber auch bei den Menschen hier im Stadtteil. Er
-					vermittelte: „das ist eine Idee, aus der wird was und Ihr schafft das und wir stehen mit Geld
-					und Vorschlägen an Eurer Seite.“<br>
-					So entstand ein vielschichtiges musikalisches Angebot, das in Gruppen stattfindet,
-					wohnortnah und auch für Familien mit geringerem Einkommen erschwinglich ist.<br>
-					Auf diesem Weg werden auch diejenigen gefördert, die erschwerte Zugangsmöglichkeiten zu
-					Musikinstrumenten haben, Interesse und Talent jedoch mitbringen. Inklusion ist selbstredend.
-					Musica Altona versteht sich zudem als Plattform des sozialen und künstlerischen Austausches
-					in einer multikulturellen Stadt, die eine weltoffene Persönlichkeitsbildung der Einzelnen
-					unterstützt.<br>
-					„Vor 20 Jahren traf sich eine Gruppe unterschiedlicher Menschen drüben in der KITA
-					Hospitalstraße im August-Lüttgens Park und gründete Musica Altona.
-					Die Idee dabei war, Kindern eine Freizeitalternative zu bieten. Damals sah es nämlich
-					in der Nachmittags-und Freizeitbetreuung noch sehr mager aus. Viele Kinder und Jugendliche
-					„daddelten“ ziellos durch den Stadtteil oder versumpften zu Hause alleine vor den
-					Fernsehern, guckten Videos...“<br>
-					<span class="about-section__quote">Marion Wolf-Dietrich (Vorstand) </span>
+					Ein vielseitiges Musikprojekt in der Nachbarschaft
+					Der Musica Altona e. V. wurde 2002 von der BürgerStiftung Hamburg zusammen mit einigen Institutionen und engagierten Musiker*innen in Altona-Altstadt initiiert, um Kindern und Jugendlichen in einem interkulturellen Rahmen das Musizieren zu ermöglichen. Ohne die kontinuierliche Unterstützung von der Bürgerstiftung Hamburg hätte es dieses Projekt nicht geben können. Ein besonderer Dank an <span class="name">Hugbert Flitner</span>, der sich damals unermüdlich auf allen Ebenen dafür einsetzte - bei der Stiftung, aber auch bei den Menschen hier im Stadtteil. Er vermittelte: „das ist eine Idee, aus der wird was und Ihr schafft das und wir stehen mit Geld und Vorschlägen an Eurer Seite.“
+					So entstand ein vielschichtiges musikalisches Angebot, das in Gruppen stattfindet, wohnortnah und auch für Familien mit geringerem Einkommen erschwinglich ist.
+					Auf diesem Weg werden auch diejenigen gefördert, die erschwerte Zugangsmöglichkeiten zu Musikinstrumenten haben, Interesse und Talent jedoch mitbringen. Inklusion ist selbstredend. Musica Altona versteht sich zudem als Plattform des sozialen und künstlerischen Austausches in einer multikulturellen Stadt, die eine weltoffene Persönlichkeitsbildung der Einzelnen unterstützt.
+					„Vor 20 Jahren traf sich eine Gruppe unterschiedlicher Menschen drüben in der KITA Hospitalstraße im August-Lüttgens Park und gründete Musica Altona. Die Idee dabei war, Kindern eine Freizeitalternative zu bieten. Damals sah es nämlich in der Nachmittags-und Freizeitbetreuung noch sehr mager aus. Viele Kinder und Jugendliche „daddelten“ ziellos durch den Stadtteil oder versumpften zu Hause alleine vor den Fernsehern, guckten Videos...“<br>
+					<span class="about-section__quote">Marion Wolf-Dietrich (Vorstand)</span>
 				</p>
 			</div>
 		</div>
@@ -43,37 +27,19 @@ get_header();
 			<div class="col-sm-12">
 				<h2 class="pb-3" data-aos="zoom-in">Die Kraft der Musik</h2>
 				<p data-aos="zoom-in-up">
-					„Wir von Musica Altona möchten Kindern ein Fenster zur Musik öffnen, die sonst keinen
-					Zugang dazu hätten. Dabei ist es uns wichtig, mit Spaß und Freude am Musizieren
-					Kinder und Jugendliche in ihrem Selbstvertrauen zu stärken. Jeder Auftritt und der Applaus,
-					aber auch schon die kleinen Erfolge im Unterricht, bringen die Kinder voran – z.B. wenn es
-					plötzlich nach einigem Üben 'klingt' und manchmal sogar richtig einen 'Flow' schafft. Wir sind
-					überzeugt, dass die Kraft der Musik und des gemeinsamen Musizierens etwas Besonderes ist.“<br>
+					„Wir von Musica Altona möchten Kindern ein Fenster zur Musik öffnen, die sonst keinen Zugang dazu hätten. Dabei ist es uns wichtig, mit Spaß und Freude am Musizieren Kinder und Jugendliche in ihrem Selbstvertrauen zu stärken. Jeder Auftritt und der Applaus, aber auch schon die kleinen Erfolge im Unterricht, bringen die Kinder voran – z.B. wenn es plötzlich nach einigem Üben 'klingt' und manchmal sogar richtig einen 'Flow' schafft. Wir sind überzeugt, dass die Kraft der Musik und des gemeinsamen Musizierens etwas Besonderes ist.“<br>
 					<span class="about-section__quote">Patricia Renz (Vorstandsvorsitz)</span>
 				</p>
 				<p>
-					Unter der Leitung von erfahrenen Dozenten:innen und Musikpädagogen:innen treffen sich die
-					Schüler:innen in Altonaer Schulen und anderen Einrichtungen. Auf einen einzigen Musikstil
-					müssen sich die inzwischen über 300 Nachwuchsmusiker:innen nicht festlegen, denn bei
-					Musica Altona wird in verschiedenen Gruppen »Crossover« musiziert:
-					von Schlagzeug, Blasinstrumenten, Klavier, Keyboard, afrikanischen Trommeln über Saz,
-					Ukulele und Gitarre bis zur klassischen Geige, Cello sowie einem Shanty Chor und
-					Band Projekten.
+					Unter der Leitung von erfahrenen Dozenten*innen und Musikpädagogen*innen treffen sich die Schülerinnen und Schüler in Altonaer Schulen und anderen Einrichtungen, wie zum Beispiel dem Jugendkunsthaus ESCHE. Auf einen einzigen Musikstil müssen sich die inzwischen über 300 Nachwuchsmusiker*innen nicht festlegen, denn bei Musica Altona wird in verschiedenen Gruppen »Crossover« musiziert: von Schlagzeug, Blasinstrumenten, Klavier, Keyboard, Trommeln, Saz, Ukulele und Gitarre bis zur klassischen Geige & Bratsche, Cello, Musiktheorie-und Gehörbildung sowie Band Projekten.
 				</p>
 				<p>
-					„Ob Elternstunde oder unser KinderMusikfest - für manche Kinder ist es der erste Auftritt,
-					andere sind schon länger dabei. Bei allem steht aber immer der Spaß an erster Stelle. Es geht
-					nicht um eine perfekte Darbietung, sondern um den Mut aufzutreten und etwas von dem
-					Erlernten zeigen zu wollen.“<br>
+					„Ob Elternstunde oder unser KinderMusikfest - für manche Kinder ist es der erste Auftritt, andere sind schon länger dabei. Bei allem steht aber immer der Spaß an erster Stelle. Es geht nicht um eine perfekte Darbietung, sondern um den Mut aufzutreten und etwas von dem Erlernten zeigen zu wollen.“<br>
 					<span class="about-section__quote">Bethina Walbaum (Vorstand)</span>
 				</p>
 				<p>
-					Mit dem 'Rollenden Musikkoffer', der regelmäßig durch die Kitas des Stadtteils zieht, erleben
-					schon die ganz Kleinen seit vielen Jahren ein lebendiges Programm der musikalischen
-					Früherziehung. Anfang 2019 kam 'Bellas Klangtonne' dazu, ein Projekt für die Elementar
-					Kinder in Kitas, im März 2022 folgte 'Bella spielt Ukulele'.<br>
-					Bereits hier wird das Prinzip des Gruppenunterrichts gepflegt, der sich dann für Kinder ab dem
-					ersten Schuljahr in den Instrumental- und natürlich auch Orchestergruppen fortsetzt.
+					Mit dem 'Rollenden Musikkoffer', der regelmäßig durch die Kitas des Stadtteils zieht, erleben schon die ganz Kleinen seit vielen Jahren ein lebendiges Programm der musikalischen Früherziehung. Anfang 2019 kam 'Bellas Klangtonne' dazu, ein Projekt für die Elementar Kinder in Kitas, im März 2022 folgte 'Bella spielt Ukulele'.
+					Bereits hier wird das Prinzip des Gruppenunterrichts gepflegt, der sich dann für Kinder ab dem ersten Schuljahr in den Instrumental- und natürlich auch Orchestergruppen fortsetzt.
 				</p>
 			</div>
 		</div>
@@ -81,10 +47,7 @@ get_header();
 			<div class="col-sm-12">
 				<h2 class="pb-3" data-aos="zoom-in">zu guter Letzt</h2>
 				<p class="pb-3" data-aos="zoom-in-up">
-					Seit 2002 ist der große und von Herzen kommende Einsatz aller Beteiligten bezeichnend für
-					Musica Altona. Die Kursgebühren sind moderat und die Organisation schlank.
-					So wird es jungen Menschen ermöglicht - auch denjenigen, die neu in der Stadt sind - einen
-					leichteren Zugang zu ihrem Stadtteil und anderen Jugendlichen zu finden.
+					Seit 2002 ist der große und von Herzen kommende Einsatz aller Beteiligten bezeichnend für Musica Altona. Die Kursgebühren sind moderat und die Organisation schlank. So wird es jungen Menschen ermöglicht - auch denjenigen, die neu in der Stadt sind - einen leichteren Zugang zu ihrem Stadtteil und anderen Jugendlichen zu finden.
 				</p>
 			</div>
 		</div>
